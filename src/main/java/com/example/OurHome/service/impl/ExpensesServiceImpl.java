7fad500d3dc79871c44774b729d6b34ed8726c56
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-11-30 22:30:35 +0200

    Create ExpensesServiceImpl.java

---
+++ src/main/java/com/example/OurHome/service/impl/ExpensesServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.OurHome.service.impl;
+
+import com.example.OurHome.service.ExpensesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpensesServiceImpl implements ExpensesService {
+
+}
