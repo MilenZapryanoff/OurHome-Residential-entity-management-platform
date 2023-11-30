b42ae747ce05b806e31f02a5e8781ae85ee59e22
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-11-30 22:30:09 +0200

    Update PropertyFeeService.java

--- src/main/java/com/example/OurHome/service/PropertyFeeService.java
+++ src/main/java/com/example/OurHome/service/PropertyFeeService.java
@@ -2,13 +2,18 @@ package com.example.OurHome.service;
 
 import com.example.OurHome.model.Entity.Property;
 import com.example.OurHome.model.Entity.PropertyFee;
+import com.example.OurHome.model.Entity.dto.BindingModels.PropertyFee.PropertyFeeAddBindingModel;
 import com.example.OurHome.model.Entity.dto.BindingModels.PropertyFee.PropertyFeeEditBindingModel;
 
+import java.math.BigDecimal;
+
 public interface PropertyFeeService {
-    void createFirstFee(Property property);
-    void createNewFee(Property property);
+    BigDecimal checkTotalDueAmount(Long id);
     PropertyFee findPropertyFeeById(Long propertyFeeId);
     PropertyFeeEditBindingModel mapPropertyFeeToBindingModel(Long id);
+    void createFirstFee(Property property);
+    void createMonthlyFee(Property property);
     void modifyFee(Long propertyFeeId, PropertyFeeEditBindingModel propertyFeeEditBindingModel);
     void deleteFee(PropertyFee propertyFee);
+    void addFee(Property property, PropertyFeeAddBindingModel propertyFeeAddBindingModel);
 }
