1cdaf95f77b0291258f8f9caf79cf94a40ea8a07
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-12-02 01:04:58 +0200

    Update ExpensesService.java

--- src/main/java/com/example/OurHome/service/ExpensesService.java
+++ src/main/java/com/example/OurHome/service/ExpensesService.java
@@ -1,5 +1,26 @@
 package com.example.OurHome.service;
 
+import com.example.OurHome.model.Entity.Expense;
+import com.example.OurHome.model.Entity.ResidentialEntity;
+import com.example.OurHome.model.Entity.dto.BindingModels.Expense.ExpenseAddBindingModel;
+import com.example.OurHome.model.Entity.dto.BindingModels.Expense.ExpenseEditBindingModel;
+import com.example.OurHome.model.Entity.dto.BindingModels.Expense.ExpenseFilterBindingModel;
+
+import java.time.LocalDate;
+
 public interface ExpensesService {
 
+    Expense findById(Long id);
+
+    void createExpense(ResidentialEntity residentialEntity, ExpenseAddBindingModel expenseAddBindingModel);
+
+    ExpenseEditBindingModel mapExpenseToBindingModel(Expense expense);
+
+    void editExpense(ExpenseEditBindingModel expenseEditBindingModel, Expense expense);
+
+    void deleteExpense(Expense expense);
+
+    ExpenseFilterBindingModel createDefaultExpenseFilter(ResidentialEntity residentialEntity);
+
+    ExpenseFilterBindingModel createCustomExpenseFilter(LocalDate startPeriod, LocalDate endPeriod, ResidentialEntity residentialEntity);
 }
