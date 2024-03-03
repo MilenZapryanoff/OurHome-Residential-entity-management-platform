74746adf2e96f21f987df7d62abc37c7dde511a7
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-03-03 14:35:01 +0200

    Update OverpaymentBindingModel.java

--- src/main/java/com/example/OurHome/model/dto/BindingModels/PropertyFee/OverpaymentBindingModel.java
+++ src/main/java/com/example/OurHome/model/dto/BindingModels/PropertyFee/OverpaymentBindingModel.java
@@ -1,14 +1,16 @@
 package com.example.OurHome.model.dto.BindingModels.PropertyFee;
 
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public class OverpaymentBindingModel {
 
-    @DecimalMin(value = "0.0", message = "Fixed Fee must be greater than or equal to zero")
+    @DecimalMin(value = "0.0", message = "Overpayment amount must be greater than or equal to zero")
     private BigDecimal overPayment;
+
+    @DecimalMin(value = "0.0", message = "Additional fee amount must be greater than or equal to zero")
+    private BigDecimal additionalPropertyFee;
 
     public BigDecimal getOverPayment() {
         return overPayment;
@@ -16,5 +18,13 @@ public class OverpaymentBindingModel {
 
     public void setOverPayment(BigDecimal overPayment) {
         this.overPayment = overPayment;
+    }
+
+    public BigDecimal getAdditionalPropertyFee() {
+        return additionalPropertyFee;
+    }
+
+    public void setAdditionalPropertyFee(BigDecimal additionalPropertyFee) {
+        this.additionalPropertyFee = additionalPropertyFee;
     }
 }
