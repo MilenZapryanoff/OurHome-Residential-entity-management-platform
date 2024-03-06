247a65ff680eb2a6f6209c6a4507a4cf3b86d087
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-03-06 22:17:35 +0200

    Update accessCode-hide.js

--- src/main/resources/static/js/accessCode-hide.js
+++ src/main/resources/static/js/accessCode-hide.js
@@ -1,10 +1,43 @@
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#residentialAccessCode');
 
-togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('fa-eye-slash');
+let isMouseDown = false;
+
+togglePassword.addEventListener('mousedown', function (e) {
+    isMouseDown = true;
+    // Show the password
+    password.setAttribute('type', 'text');
+    // Change the eye icon to hide
+    togglePassword.classList.remove('fa-eye-slash');
+    togglePassword.classList.add('fa-eye');
+    togglePassword.classList.remove('text-danger');
+    togglePassword.classList.add('text-success');
+
+});
+
+// When mouse leaves the icon area, hide the password if the left button is not held down
+togglePassword.addEventListener('mouseleave', function (e) {
+    if (!e.buttons) {
+        // Hide the password
+        password.setAttribute('type', 'password');
+        isMouseDown = false;
+        // Change the eye icon to show
+        togglePassword.classList.remove('fa-eye');
+        togglePassword.classList.add('fa-eye-slash');
+    }
+});
+
+// When the mouse is released, hide the password
+document.addEventListener('mouseup', function (e) {
+    if (isMouseDown) {
+        // Hide the password
+        password.setAttribute('type', 'password');
+        isMouseDown = false;
+        // Change the eye icon to show and color
+        togglePassword.classList.remove('fa-eye');
+        togglePassword.classList.add('fa-eye-slash');
+        togglePassword.classList.remove('text-success');
+        togglePassword.classList.add('text-danger');
+
+    }
 });
