044246a6d1ad88b25cc86f3fb44d0fbf23937a14
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-03-12 16:02:06 +0200

    Update PropertyFeeService.java

--- src/main/java/com/example/OurHome/service/PropertyFeeService.java
+++ src/main/java/com/example/OurHome/service/PropertyFeeService.java
@@ -3,7 +3,6 @@ package com.example.OurHome.service;
 import com.example.OurHome.model.Entity.Property;
 import com.example.OurHome.model.Entity.PropertyFee;
 import com.example.OurHome.model.Entity.ResidentialEntity;
-import com.example.OurHome.model.dto.BindingModels.PropertyFee.OverpaymentBindingModel;
 import com.example.OurHome.model.dto.BindingModels.PropertyFee.PropertyFeeAddBindingModel;
 import com.example.OurHome.model.dto.BindingModels.PropertyFee.PropertyFeeAddGlobalFeeBindingModel;
 import com.example.OurHome.model.dto.BindingModels.PropertyFee.PropertyFeeEditBindingModel;
@@ -16,11 +15,11 @@ public interface PropertyFeeService {
     PropertyFee findPropertyFeeById(Long propertyFeeId);
     PropertyFeeEditBindingModel mapPropertyFeeToBindingModel(Long id);
     void createFirstFee(PropertyApprovalEvent propertyApprovalEvent);
-    void createMonthlyFee(Property property);
-    void modifyFee(Long propertyFeeId, PropertyFeeEditBindingModel propertyFeeEditBindingModel);
-    void deleteFee(PropertyFee propertyFee);
-    void addFee(Property property, PropertyFeeAddBindingModel propertyFeeAddBindingModel);
+    void createPeriodicalMonthlyFee(Property property);
+    void editMonthlyFee(Long propertyFeeId, PropertyFeeEditBindingModel propertyFeeEditBindingModel);
+    void deleteMonthlyFee(PropertyFee propertyFee);
+    void createSingleFee(Property property, PropertyFeeAddBindingModel propertyFeeAddBindingModel);
     void changePaymentStatus(PropertyFee propertyFee);
-    boolean addGlobalFee(ResidentialEntity residentialEntity, PropertyFeeAddGlobalFeeBindingModel propertyFeeAddGlobalFeeBindingModel);
+    boolean createMassFee(ResidentialEntity residentialEntity, PropertyFeeAddGlobalFeeBindingModel propertyFeeAddGlobalFeeBindingModel);
 
 }
