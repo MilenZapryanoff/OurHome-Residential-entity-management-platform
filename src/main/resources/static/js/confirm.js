10fd08dd621c6f0a6a40616665dbb8a517f93771
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-09-29 12:44:23 +0300

    update

--- src/main/resources/static/js/confirm.js
+++ src/main/resources/static/js/confirm.js
@@ -1,7 +1,4 @@
-function confirmCancelChange() {
-    return confirm("Are you sure you want to Cancel this change request?");
-}
-
+//non-animated buttons with DEFAULT browser pop-up confirmation window
 function confirmSendMessage() {
     return confirm("Are you sure you want to send this message?");
 }
@@ -12,6 +9,51 @@ function confirmUnlinkOwner() {
         "Property owner will have to make a new registration request to obtain access of his property data");
 }
 
+
+//non-animated buttons with CUSTOM pop-up confirmation window
+document.getElementById('delete-btn').addEventListener('click', function() {
+    // Show the custom modal
+    document.querySelector('.modal').style.display = 'block';
+});
+
+document.getElementById('yes-btn').addEventListener('click', function() {
+    // Submit the form when 'Yes' is clicked
+    document.getElementById('form-confirm').submit();
+    // Close the modal
+    document.querySelector('.modal').style.display = 'none';
+});
+
+document.getElementById('no-btn').addEventListener('click', function() {
+    // Simply close the modal when 'No' is clicked
+    document.querySelector('.modal').style.display = 'none';
+});
+
+
+
+//html code
+<!--custom pop-up confirmation window before <form>-->
+/*
+<div class="modal">
+    <div class="modal-content">
+        <strong class="text-danger-emphasis">Attention!</strong>
+        <p class="text-dark">Do you really want to remove property owner?</p>
+        <small class="text-secondary">Please note, that this action is not reversible!Property owner will have to make a new registration request to obtain access of his property data.</small>
+        <div class="confirm-buttons-container">
+            <button id="yes-btn">Confirm</button>
+            <button id="no-btn">Cancel</button>
+        </div>
+    </div>
+</div>
+
+
+//form has to be with id="form-confirm"
+//button has to be with id="delete-btn"
+
+*/
+
+
+
+//Monthly fees ALERTS custom pop-up notification
 function openCustomDialog(id, message) {
     const dialogTextId = 'customDialogText_' + id;
     const dialogId = 'customDialog_' + id;
